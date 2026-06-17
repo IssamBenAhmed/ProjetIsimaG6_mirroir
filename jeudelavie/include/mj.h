2a115f5bdbd155a720166e5d584ee4e900f5d240
Gahui BAN <gahui.ban@etu.uca.fr>
2026-06-17 10:11:44 +0000

    Edit mj.h

--- jeudelavie/include/mj.h
+++ jeudelavie/include/mj.h
@@ -2,13 +2,11 @@
 #define MAITRE_JEU
 
 
-#include <SDL2/SDL.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#define WIDTH 20 ;
-#define HEIGHT 20;
+
 
 
 int compter_voisins(int grille[WIDTH][HEIGHT], int x, int y);
