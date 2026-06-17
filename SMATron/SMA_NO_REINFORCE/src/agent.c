c774966a46dc690c9d5a58aa93862abab5abb089
IssamBenAhmed <benahmed.issam7@gmail.com>
2026-06-17 18:38:36 +0200

    agent implémenté

--- SMATron/SMA_NO_REINFORCE/src/agent.c
+++ SMATron/SMA_NO_REINFORCE/src/agent.c
@@ -1,7 +1,19 @@
 #include "../include/agent.h"
 
 int choisir_action(float perception[PERCEPTION_SIZE]) {
-	(void)perception;
 
-	return ACTION_FORWARD;
+    int best_action = ACTION_FORWARD; 
+    float max_espace = perception[0]; 
+
+    if (perception[1] > max_espace) {
+        max_espace = perception[1];
+        best_action = ACTION_LEFT;
+    }
+
+    if (perception[2] > max_espace) {
+        max_espace = perception[2];
+        best_action = ACTION_RIGHT;
+    }
+
+    return best_action;
 }
