62c76fe345a0e25ff8a270529c0c9ef69faae0a7
Gahui BAN <gahui.ban@etu.uca.fr>
2026-06-18 08:29:38 +0000

    Edit mj.c

--- SMATron/SMA_NO_REINFORCE/src/mj.c
+++ SMATron/SMA_NO_REINFORCE/src/mj.c
@@ -1,21 +1,110 @@
 #include "../include/mj.h"
+#include "agent.h"
 
 void calculer_perception(int grille[WIDTH][HEIGHT], int x, int y, int direction, float perception[PERCEPTION_SIZE]) {
-	(void)grille;
-	(void)x;
-	(void)y;
-	(void)direction;
-	(void)perception;
+	
+	int devant = 0, gauche = 0, droite = 0; 
+
+    int dx, dy;
+
+    // vecteur devant selon direction de moto
+    if(direction == 0){ dx = 1; dy = 0; }
+    else if(direction == 1){ dx = -1; dy = 0; }
+    else if(direction == 2){ dx = 0; dy = 1; }
+    else { dx = 0; dy = -1; }
+
+    // gauche = rotation -90°
+    int ldx = -dy;
+    int ldy = dx;
+
+    // droite = rotation +90°
+    int rdx = dy;
+    int rdy = -dx;
+
+    // perception devant
+    for(int i = 1; i < WIDTH; i++){
+        int nx = x + i * dx; //calcul de coordonnée de case devant de moto
+        int ny = y + i * dy;
+
+        if(nx < 0 || nx >= WIDTH || ny < 0 || ny >= HEIGHT) break;//quand la case est hors de zone , on s'arrete
+        if(grille[nx][ny] != 0) break; //quand la case est bloqué , on s'arrete
+
+        devant++;
+    }
+
+    // gauche
+    for(int i = 1; i < WIDTH; i++){
+        int nx = x + i * ldx; //calcul de coordonnée de case gauche de moto
+        int ny = y + i * ldy;
+
+        if(nx < 0 || nx >= WIDTH || ny < 0 || ny >= HEIGHT) break;//quand la case est hors de zone , on s'arrete
+        if(grille[nx][ny] != 0) break; //quand la case est bloqué , on s'arrete
+
+
+        gauche++;
+    }
+
+    // droite
+    for(int i = 1; i < WIDTH; i++){
+        int nx = x + i * rdx;//calcul de coordonnée de case droite de moto
+        int ny = y + i * rdy;
+
+        if(nx < 0 || nx >= WIDTH || ny < 0 || ny >= HEIGHT) break;//quand la case est hors de zone , on s'arrete
+        if(grille[nx][ny] != 0) break; //quand la case est bloqué , on s'arrete
+
+
+        droite++;
+    }
+
+    perception[0] = devant;
+    perception[1] = gauche;
+    perception[2] = droite;
 }
 
 void mettre_a_jour_monde(int grille[WIDTH][HEIGHT], int pos_motos[MAX_MOTOS + 1][2], int dir_motos[MAX_MOTOS + 1], bool etats_vie[MAX_MOTOS + 1]) {
-	(void)grille;
-	(void)pos_motos;
-	(void)dir_motos;
-	(void)etats_vie;
+	int i = 0;
+    for(i = 0; i < MAX_MOTOS+1; i++){
+       
+    int x = pos_motos[i][0] ;
+    int y = pos_motos[i][1];
+    int direction = dir_motos[i];
+    float perception[3]={0};
+    calculer_perception(grille, x, y,direction, perception); //on regarde si à la vue de moto, il y a des places libres
+    if (perception[0] ==0 && perception[1] ==0 && perception[2] ==0){//il est bloqué partout
+            etats_vie[i] = 0; //le moto mort 
+            nettoyer_trainee(grille, i); // on nettoie son trajet
+    }
+    else {
+        	int action = choisir_action(perception); 
+            //agent choisi, en fonction de sa perception, à quelle action qu'il va choisir : 0= tout droite ; 1 = gauche ; 2 = droite
+           if (action == 0 && x > 0) {
+                grille[x-1][y] = i;
+                pos_motos[i][0] = x-1;
+            }
+            if (action == 1 && y > 0) {
+                grille[x][y-1] = i;
+                pos_motos[i][1] = y-1;
+            }
+            if (action == 2 && y < HEIGHT - 1) {
+                grille[x][y+1] = i;
+                pos_motos[i][1] = y+1;
+
+
+            }      
+
+        }
+
+    }
+
 }
 
 void nettoyer_trainee(int grille[WIDTH][HEIGHT], int id_moto) {
-	(void)grille;
-	(void)id_moto;
+	for(int i = 0 ; i < WIDTH ; i++){
+        for(int j = 0 ; j < HEIGHT ; j++){
+            if( grille[i][j] == id_moto){ //si il y a le trajet de moto mort, on le vide
+                grille[i][j] == 0;
+            }
+        }
+    }
+
 }
