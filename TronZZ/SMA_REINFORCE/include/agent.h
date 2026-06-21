21f520c26ace6d57d8837064451863ece3dc6f2a
imbenahmed <Issam_Malek.BEN_AHMED@etu.uca.fr>
2026-06-21 16:11:06 +0200

    implémentation d'une structure comme argument de la fonction de décison de l'agent, format plus facile à débug, ey plus efficace d'un pt de vue génie logiciel

---
+++ TronZZ/SMA_REINFORCE/include/agent.h
@@ -0,0 +1,20 @@
+#ifndef AGENT_H
+#define AGENT_H
+
+#include "config.h"
+
+float random_float(void);
+int choisir_action(float perception[PERCEPTION_SIZE]);
+
+typedef struct {
+    // Module 1 : Vision linéaire de précision (Les rayons)
+    float distances_murs[3]; // [0]=Devant, [1]=Gauche, [2]=Droite
+    
+    // Module 2 : Vision globale "Floue" (Les zones)
+    float densite_obstacles[4]; // [0]=Avant-G, [1]=Avant-D, [2]=Arriere-G, [3]=Arriere-D
+    int zone_adversaire_1;      // ID de la zone (0 à 3) où se trouve l'adversaire
+    int zone_adversaire_2;
+    int zone_adversaire_3;
+} Perception;
+
+#endif
