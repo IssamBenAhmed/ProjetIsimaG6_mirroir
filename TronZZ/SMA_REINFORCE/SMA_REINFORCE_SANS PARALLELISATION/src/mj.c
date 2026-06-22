4c9a0cc5f6764a00c3b9bced972f56049e540cb1
Gahui BAN <gahui.ban@etu.uca.fr>
2026-06-22 14:31:04 +0000

    Edit mj.c

--- TronZZ/SMA_REINFORCE/SMA_REINFORCE_SANS PARALLELISATION/src/mj.c
+++ TronZZ/SMA_REINFORCE/SMA_REINFORCE_SANS PARALLELISATION/src/mj.c
@@ -234,7 +234,7 @@ void nettoyer_trainee(int grille[WIDTH][HEIGHT], int id_moto) {
     }
 }
 
-void mettre_a_jour_monde(int grille[WIDTH][HEIGHT], int pos_motos[MAX_MOTOS + 1][2], int dir_motos[MAX_MOTOS + 1], bool etats_vie[MAX_MOTOS + 1]) {
+void mettre_a_jour_monde(int grille[WIDTH][HEIGHT], int pos_motos[MAX_MOTOS + 1][2], int dir_motos[MAX_MOTOS + 1], bool etats_vie[MAX_MOTOS + 1], FrameMemoire * frame) {
     /*int gagner_ou_pas = trouver_gagnant(etats_vie) ;
     if (gagner_ou_pas !=-1){
         printf("le gagnant est %d \n", gagner_ou_pas);
@@ -254,7 +254,7 @@ void mettre_a_jour_monde(int grille[WIDTH][HEIGHT], int pos_motos[MAX_MOTOS + 1]
             Perception * perception = malloc(sizeof(Perception));
             calculer_perception(grille, x, y, dir_motos[i], perception);
 
-            int action = choisir_action(perception); //venant de "agent.h"
+            int action = choisir_action(perception, frame); //venant de "agent.h"
 
             if (action == ACTION_LEFT) {
                 dir_motos[i] = (dir_motos[i] + 3) % 4; // Rotation -90°
