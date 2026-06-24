c8ae2afe639bdda41facf09cb1b9bdbe98a918c8
Gahui BAN <gahui.ban@etu.uca.fr>
2026-06-24 08:49:01 +0000

    Edit mj.h

--- TronZZ/SMA_REINFORCE/SMA_REINFORCE_SANS PARALLELISATION/include/mj.h
+++ TronZZ/SMA_REINFORCE/SMA_REINFORCE_SANS PARALLELISATION/include/mj.h
@@ -24,9 +24,8 @@ bool gagnant_ou_pas(bool etats_vie[MAX_MOTOS + 1], EpisodeMemoire memoires[MAX_M
 
 int cause_mort( int grille[WIDTH][HEIGHT],  int x , int y); 
 
-int existence_adversaire_dans_un_zone ( int grille[WIDTH][HEIGHT],int x,int y,int dx1, int dy1, int dx2,int dy2 ) ;
-
-void calculer_perception(int grille[WIDTH][HEIGHT], int x, int y, int direction, Perception * perception ) ;
+int existence_adversaire_dans_un_zone(int grille[WIDTH][HEIGHT], int x, int y, int dx1, int dy1, int dx2, int dy2, int mon_id);
+void calculer_perception(int grille[WIDTH][HEIGHT], int x, int y, int direction, int mon_id, Perception * perception);
 
 void mettre_a_jour_monde(int grille[WIDTH][HEIGHT], int pos_motos[MAX_MOTOS + 1][2], int dir_motos[MAX_MOTOS + 1], bool etats_vie[MAX_MOTOS + 1], EpisodeMemoire memoires[MAX_MOTOS + 1]); 
 
