382fb8e178cb2342f8cb0326fedc6439739063c9
Nabil LAAMARCHI <nabil.laamarchi@etu.uca.fr>
2026-06-25 07:30:19 +0000

    Edit graphisme.h

--- TronZZ/SMA_REINFORCE/SMA_REINFORCE_AVEC_PARALLELISATION/include/graphisme.h
+++ TronZZ/SMA_REINFORCE/SMA_REINFORCE_AVEC_PARALLELISATION/include/graphisme.h
@@ -1,10 +1,20 @@
 #ifndef GRAPHISME_H
 #define GRAPHISME_H
 
-#include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <SDL2/SDL.h>
 
 #include "config.h"
+
+/*
+ * Charge les images des motos.
+ */
+bool initialiser_textures_motos(SDL_Renderer *renderer);
+
+/*
+ * Libere les textures des motos.
+ */
+void liberer_textures_motos(void);
 
 /*
  * Affiche l'écran titre avant le début du jeu.
@@ -16,13 +26,16 @@ bool afficher_ecran_titre(SDL_Window *window, SDL_Renderer *renderer);
 /*
  * Dessine l'arène du jeu.
  */
-void dessiner_arene(SDL_Renderer *renderer, int grille[WIDTH][HEIGHT]);
+void dessiner_arene(SDL_Renderer *renderer,
+                    int grille[WIDTH][HEIGHT],
+                    int pos_motos[MAX_MOTOS + 1][2],
+                    int dir_motos[MAX_MOTOS + 1],
+                    bool etats_vie[MAX_MOTOS + 1]);
 
 /*
  * Gère les événements clavier du joueur.
  */
-void capturer_evenements(SDL_Event *event, int *direction_joueur,
-                         bool *running);
+void capturer_evenements(SDL_Event *event, int *direction_joueur, bool *running);
 
 /*
  * Dessine le panneau final.
